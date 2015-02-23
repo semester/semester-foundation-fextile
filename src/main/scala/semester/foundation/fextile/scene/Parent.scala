8b27b9fbce96583391becf20c2853472c5925c76
Takayuki Okazaki <takayuki.okazaki@gmail.com>
2015-02-24 06:33:32 +0900

    organize imports

--- src/main/scala/semester/foundation/fextile/scene/Parent.scala
+++ src/main/scala/semester/foundation/fextile/scene/Parent.scala
@@ -1,11 +1,11 @@
 package semester.foundation.fextile.scene
 
-import javafx.scene.{Parent => FXParent}
+import javafx.{scene => fxs}
 
 import semester.foundation.fextile.boundary.FextileDelegate
 
 import scala.concurrent.Future
 
-class Parent extends FextileDelegate[FXParent] {
-  override val delegate: Future[FXParent] = _
+class Parent extends FextileDelegate[fxs.Parent] {
+  override val delegate: Future[fxs.Parent] = _
 }
